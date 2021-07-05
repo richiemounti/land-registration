d9550199957e5f8de90d31797d14e4ab135b5264
riciemounti <babtdaddy67@gmail.com>
2021-07-05 18:05:32 +0300

    land registration update

---
+++ frontend/enrolladmin.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const FabricCAServices = require('fabric-ca-client');
+const { Wallets } = require('fabrc-network');
+const fs = require('fs');
+const path = require('path');
+
+async function
